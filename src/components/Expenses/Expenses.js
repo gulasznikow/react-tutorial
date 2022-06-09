b0c37cb64ee0b3910bc3ebeffac05ce84b3d2cf6
Maciej Sajdok <maciej.sajdok@tsh.io>
2022-06-09 19:31:19 +0200

    62

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [year, setYear] = useState('2019');
+
+  const onChangeFilterHandler = (selectedYear) => {
+    setYear(selectedYear);
+    console.log(selectedYear);
+  }
+
   const expenses = props.expenses.map((expense) => {
     return (
       <ExpenseItem
@@ -13,7 +22,10 @@ const Expenses = (props) => {
     );
   });
 
-  return <Card className="expenses">{expenses}</Card>;
+  return <Card className="expenses">
+    <ExpensesFilter selected={year} onChangeFilter={onChangeFilterHandler} />
+    {expenses}
+    </Card>;
 };
 
 export default Expenses;
