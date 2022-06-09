549b9dc0994da7bd66e0ac8b34b099a450bce880
Maciej Sajdok <maciej.sajdok@tsh.io>
2022-06-09 19:39:24 +0200

    64

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
     <div>
       <h2>Let's get started!</h2>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,28 +4,38 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
-const Expenses = (props) => {
-  const [year, setYear] = useState('2019');
+const Expenses = ({ items }) => {
+  const [year, setYear] = useState("2019");
 
   const onChangeFilterHandler = (selectedYear) => {
     setYear(selectedYear);
     console.log(selectedYear);
-  }
+  };
 
-  const expenses = props.expenses.map((expense) => {
-    return (
-      <ExpenseItem
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    );
-  });
+  // const expenses = props.expenses.map((expense) => {
+  //   return (
+  //     <ExpenseItem
+  //       title={expense.title}
+  //       amount={expense.amount}
+  //       date={expense.date}
+  //     />
+  //   );
+  // });
 
-  return <Card className="expenses">
-    <ExpensesFilter selected={year} onChangeFilter={onChangeFilterHandler} />
-    {expenses}
-    </Card>;
+  return (
+    <Card className="expenses">
+      <ExpensesFilter selected={year} onChangeFilter={onChangeFilterHandler} />
+      {items.map((expense) => {
+        return (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        );
+      })}
+    </Card>
+  );
 };
 
 export default Expenses;
