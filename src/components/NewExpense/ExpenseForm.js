b1419db8868683d4107d712b45d495da48f96781
Maciej Sajdok <maciej.sajdok@tsh.io>
2022-06-10 22:23:36 +0200

    72

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -38,7 +38,7 @@ const ExpenseForm = ({ onSaveExpenseData, onCancelAddingExpense }) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
